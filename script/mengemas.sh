b06ec8f653b7777d6fcd514500678f665c50965b
losgw <112204132+losgw@users.noreply.github.com>
2022-12-19 03:43:51 +0700

    Update mengemas.sh

--- script/mengemas.sh
+++ script/mengemas.sh
@@ -32,6 +32,7 @@ tautan=https://royal-snowflake.regenerate.workers.dev/0:/$nama_rom/$perangkat/$n
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip build:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 cd $WORKDIR/rom/$nama_rom/out/target/product/$perangkat
+curl -T $nama_file oshi.at; echo
 
 echo -e \
 "
