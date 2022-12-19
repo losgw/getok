dcc7618257333ab7f1706fdf33fb6cffdbb1bd83
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-19 13:31:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aslenofarid/local_manifest --depth 1 -b lineage-19.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/local_manifest --depth 1 -b tissot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
@@ -9,11 +9,11 @@ timeStart
 
 source build/envsetup.sh
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
-export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
-export BUILD_USERNAME=$KBUILD_BUILD_USER
-export BUILD_HOSTNAME=$KBUILD_BUILD_HOST
-lunch lineage_X00TD-userdebug
+export KBUILD_BUILD_USER=Honoka$KBUILD_BUILD_USER
+export KBUILD_BUILD_HOST=Cloud$KBUILD_BUILD_HOST
+export BUILD_USERNAME=Honoka$KBUILD_BUILD_USER
+export BUILD_HOSTNAME=Cloud$KBUILD_BUILD_HOST
+lunch xdroid_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
