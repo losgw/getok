6e8dc276d16bbeb3a9afee63536010cb090b4ef5
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-21 01:01:28 +0700

    RAJA MODOL

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sync rom pikun
+#sync rom modol
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
