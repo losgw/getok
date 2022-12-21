0281ec0a2c24ab6f9acc283be1a403ffadaab274
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-21 11:14:59 +0700

    RAJA MODOL

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom modol
+# build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
