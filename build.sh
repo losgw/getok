01ae32be399e89256124cac7e8ce911ee271e2d9
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-21 12:37:05 +0700

    RAJA MODOL

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom well
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectKasumi/manifest -b kasumi-v1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-kasumi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -20,12 +20,12 @@ export BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export BROKEN_ENFORCE_SYSPROP_OWNER=true
 export BROKEN_MISSING_REQUIRED_MODULES=true
 export BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-lunch xdroid_tissot-userdebug
+lunch kasumi_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka xd -j8  > reading
+make bandori -j8  > reading
 
 retVal=$?
 timeEnd
