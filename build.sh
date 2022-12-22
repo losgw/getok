40dc1288b60441aa25d484bddd78744e9cc9815a
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-22 19:20:51 +0700

    PIKUN

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sync rom well
+#sync rom well pikun
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
