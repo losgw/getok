a6aca5bed3f9c794b440eef01631fefb7c7b85fe
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-22 11:22:31 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
