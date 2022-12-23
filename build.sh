563e6ee93561a29b3bf574f1aa7aa7036dd42d03
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-23 20:46:02 +0700

    RAJA MODOL

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom wel
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-banana .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
@@ -13,19 +13,18 @@ export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=Cloud
 export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=Cloud
-export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 export BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export BROKEN_ENFORCE_SYSPROP_OWNER=true
 export BROKEN_MISSING_REQUIRED_MODULES=true
 export BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-lunch banana_tissot-userdebug
+lunch aosp_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-make banana -j8  > reading
+make bacon -j8  > reading
 
 retVal=$?
 timeEnd
