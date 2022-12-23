c13a31838091b027a13fd494f611e6c7f8200448
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-23 16:56:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sync rom welll
+#sync rom well
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
