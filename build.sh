51a7709cb3c94a170ea79d6077cbc9927bbb0f3e
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-23 13:09:54 +0700

    Pake nanya

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom welll
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-xdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
@@ -13,20 +13,12 @@ export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=Cloud
 export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=Cloud
-export BUILD_BROKEN_DUP_RULES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export RELAX_USES_LIBRARY_CHECK=true
-export BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
-export BROKEN_ENFORCE_SYSPROP_OWNER=true
-export BROKEN_MISSING_REQUIRED_MODULES=true
-export BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-lunch xdroid_tissot-userdebug
+lunch derp_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-make xd -j8  > reading
+make derp -j8  > reading
 
 retVal=$?
 timeEnd
