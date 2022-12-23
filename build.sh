3cad2d2241a4f13310306afde987a18a1de4f31c
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-23 21:36:27 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sync rom wel
+#sync rom well
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
