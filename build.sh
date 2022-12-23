e934aa65217912e7ef47f62a699da4a7942eef4f
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-23 16:42:24 +0700

    RAJA MODOL

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom welll
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-derp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
@@ -13,12 +13,12 @@ export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=Cloud
 export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=Cloud
-lunch derp_tissot-userdebug
+lunch banana_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-make derp -j8  > reading
+make banana -j8  > reading
 
 retVal=$?
 timeEnd
