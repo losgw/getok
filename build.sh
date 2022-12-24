1af17c18d7d9e74de76af75f4b854e30c93579e9
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-24 08:18:21 +0700

    STFU

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom well
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-derp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom modol
@@ -18,12 +18,12 @@ export BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export BROKEN_ENFORCE_SYSPROP_OWNER=true
 export BROKEN_MISSING_REQUIRED_MODULES=true
 export BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-lunch derp_tissot-userdebug
+lunch aosp_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka derp -j8  > reading
+make bacon -j8  > reading
 retVal=$?
 timeEnd
 statusBuild
