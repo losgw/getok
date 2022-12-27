ae7e7afb9f63c885bef2d8c1cec07a687aaf9fad
losgw <112204132+losgw@users.noreply.github.com>
2022-12-27 11:38:05 +0700

    Update mengemas.sh

--- script/mengemas.sh
+++ script/mengemas.sh
@@ -31,7 +31,7 @@ tautan=https://cirrus-ci.com/build/$CIRRUS_BUILD_ID
 
 cd $WORKDIR/rom/$nama_rom/out/target/product/$perangkat
 
-curl -T $nama_file temp.sh; echo
+curl --upload-file $nama_file https://transfer.sh/$nama_file; echo
 
 echo -e \
 "
