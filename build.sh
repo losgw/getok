fea2f3d31922c7d1f2eb5dae49e1a745b3b6fd86
losgw <112204132+losgw@users.noreply.github.com>
2023-01-07 20:38:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -18,7 +18,7 @@ mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka bacon -j8  > reading
+make bacon -j8  > reading
 
 retVal=$?
 timeEnd
