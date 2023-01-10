b2a9c638e5ca3eff5f4ad06f8854773290072d58
losgw <112204132+losgw@users.noreply.github.com>
2023-01-10 10:20:09 +0700

    Hmm

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #sync rom
-repo init -u https://github.com/ForkLineageOS/android.git -b lineage-19.1
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest --depth 1 -b lineage-19.1 .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 source build/envsetup.sh
-export TARGET_FLOS=true
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
