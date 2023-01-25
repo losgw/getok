8fe15254ebb1017a41cb016c46e195c47eae8801
losgw <losgw761@gmail.com>
2023-01-25 16:15:48 +0000

    ya lagi

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/aslenofarid/local_manifest --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
+
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
