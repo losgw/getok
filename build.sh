7998b305d4b01b7f55eac29926a1c10c5e3a8201
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-01-29 17:05:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,13 +12,6 @@ export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=HonkCI
 export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=HonkCI
-export BUILD_BROKEN_DUP_RULES=true
-export RELAX_USES_LIBRARY_CHECK=true
-RELAX_USES_LIBRARY_CHECK=true
-export BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
-export BROKEN_ENFORCE_SYSPROP_OWNER=true
-export BROKEN_MISSING_REQUIRED_MODULES=true
-export BROKEN_VENDOR_PROPERTY_NAMESPACE=true
 lunch banana_tissot-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
