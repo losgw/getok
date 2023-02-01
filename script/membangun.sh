a30b589fe1b15e252c8b0ec378b5cdd339130e2b
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-02-01 15:18:36 +0700

    Update membangun.sh

--- script/membangun.sh
+++ script/membangun.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Lingkungan Utama
+# Lingkungan Utamaa
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 nama_perangkat=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
 command=$(tail $CIRRUS_WORKING_DIR/build.sh -n +$(expr $(grep '# build rom' $CIRRUS_WORKING_DIR/build.sh -n | cut -f1 -d:) - 1)| head -n -1 | grep -v '# end')
