6aa1570c2027ceedb60f2a72f15560cce8b0f3ac
losgw <losgw761@gmail.com>
2023-02-12 00:06:47 +0000

    yaelah

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/losgw/localm --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
@@ -13,11 +13,12 @@ export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
 export BUILD_USERNAME=$KBUILD_BUILD_USER
 export BUILD_HOSTNAME=$KBUILD_BUILD_HOST
+lunch lineage_X00TD-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-brunch X00TD  > reading
+mka bacon -j8  > reading
 
 retVal=$?
 timeEnd
