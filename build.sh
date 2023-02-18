aecc75eece79cb975abd82f39ad555261e220784
losgw <losgw761@gmail.com>
2023-02-18 13:05:40 +0700

    gas

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/losgw/localm --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
