247a6a91d28c21de7ba54ea9f2e062d2d3a90c8f
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-02-19 20:13:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests --depth 1 -b tissot-bananadroidCoreGApps .repo/local_manifests
+git clone https://github.com/hklknz/Local-Manifests --depth 1 -b mido-xd12L .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,7 +12,7 @@ export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=HonkCI
 export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=HonkCI
-lunch banana_tissot-userdebug
+lunch banana_mido-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
